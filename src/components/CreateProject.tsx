f0f67b0d9fd14b3e8b599038a0222ce3bafff639
Fahad <fahad865@gmail.com>
2018-04-02 23:40:12 +0200

    Update create forms

--- src/components/CreateProject.tsx
+++ src/components/CreateProject.tsx
@@ -1,40 +1,49 @@
 import * as React from 'react';
 import { Project } from '../types/index';
-import { Modal, Form, Input } from 'antd';
+import { Modal, Form, Input, InputNumber, Row, Col } from 'antd';
 import { FormComponentProps } from 'antd/lib/form/Form';
 const FormItem = Form.Item;
 
 export interface Props {
-    show: boolean;
-    project?: Project;        
-    onCreate: () => void;
-    onCancel: () => void;
+  show: boolean;
+  project?: Project;
+  onCreate: () => void;
+  onCancel: () => void;
 }
 
-function CreateProject({show, project, onCreate, onCancel, form}: Props & FormComponentProps) {
-    const { getFieldDecorator } = form;
-    return (
-        <Modal
-            visible={show}
-            title="Create a new project"
-            okText="Create"
-            onCancel={onCancel}
-            onOk={onCreate}
-        >
-            <Form layout="vertical">
-                <FormItem label="Title">
-                    {getFieldDecorator('title', {
-                    rules: [{ required: true, message: 'Please input the title of collection!' }],
-                    })(
-                    <Input />
-                    )}
-                </FormItem>
-                <FormItem label="Description">
-                    {getFieldDecorator('description')(<Input type="textarea" />)}
-                </FormItem>                
-            </Form>
-        </Modal>
-    );
+function CreateProject({ show, project, onCreate, onCancel, form }: Props & FormComponentProps) {
+  const { getFieldDecorator } = form;
+  return (
+    <Modal
+      visible={show}
+      title="Create a new project"
+      okText="Create"
+      onCancel={onCancel}
+      onOk={onCreate}
+    >
+      <Form layout="vertical">
+        <FormItem label="Name">
+          {getFieldDecorator('name', {
+            rules: [{ required: true, message: 'Please input the name of project' }],
+          })(
+            <Input />
+          )}
+        </FormItem>
+        <Row gutter={24}>
+          <Col span={12}>
+            <FormItem label="Hourly rate">
+              {getFieldDecorator('hourlyRate')(<InputNumber />)}
+            </FormItem>
+          </Col>
+          <Col span={12}>
+            <FormItem label="Currency">
+              {getFieldDecorator('currency')(<Input />)}
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
+    </Modal>
+  );
 }
 
-export default Form.create<Props>() (CreateProject);
+export default Form.create<Props>()(CreateProject);
--- src/components/CreateTimeLog.tsx
+++ src/components/CreateTimeLog.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
-import { TimeLog } from '../types/index';
-import { Modal, Form, Input } from 'antd';
+import { TimeLog, Project } from '../types/index';
+import { Modal, Form, Input, DatePicker, Select } from 'antd';
 import { FormComponentProps } from 'antd/lib/form/Form';
 const FormItem = Form.Item;
+const Option = Select.Option;
 
 export interface Props {
   show: boolean;
   timeLog?: TimeLog;
+  projects: Project[];
   onCreate: () => void;
   onCancel: () => void;
 }
 
-function CreateTimeLog({ show, timeLog, onCreate, onCancel, form }: Props & FormComponentProps) {
+function CreateTimeLog({ show, timeLog, projects, onCreate, onCancel, form }: Props & FormComponentProps) {
   const { getFieldDecorator } = form;
   return (
     <Modal
@@ -22,15 +24,47 @@ function CreateTimeLog({ show, timeLog, onCreate, onCancel, form }: Props & Form
       onOk={onCreate}
     >
       <Form layout="vertical">
-        <FormItem label="Title">
-          {getFieldDecorator('title', {
-            rules: [{ required: true, message: 'Please input the title of collection!' }],
+        <FormItem label="Description">
+          {getFieldDecorator('description', {
+            rules: [{ required: true, message: 'Please enter the log description' }],
           })(
             <Input />
           )}
         </FormItem>
-        <FormItem label="Description">
-          {getFieldDecorator('description')(<Input type="textarea" />)}
+        <FormItem label="Project">
+          {getFieldDecorator('projectId')(
+            <Select placeholder="Project">
+              {
+                projects.map((item, index) =>
+                  <Option key={index} value={item.id}>{item.name}</Option>
+                )
+              }
+            </Select>
+          )}
+        </FormItem>
+        <FormItem label="Start time">
+          {getFieldDecorator('startTime', {
+            rules: [{ required: true, message: 'Please enter the start time' }],
+          })(
+            <DatePicker
+              showTime={true}
+              format="DD-MM-YYYY HH:mm:ss"
+              placeholder="Select Time"
+              style={{ margin: '-5px 0' }}
+            />
+          )}
+        </FormItem>
+        <FormItem label="End time">
+          {getFieldDecorator('endTime', {
+            rules: [{ required: true, message: 'Please enter the end time' }],
+          })(
+            <DatePicker
+              showTime={true}
+              format="DD-MM-YYYY HH:mm:ss"
+              placeholder="Select Time"
+              style={{ margin: '-5px 0' }}
+            />
+          )}
         </FormItem>
       </Form>
     </Modal>
